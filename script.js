64e750b0c1c16fbc6c3ebb1beb952d3e286deee2
prajwal1805 <prajwalkanaka60@gmail.com>
2025-03-03 12:54:47 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    background-color: #f4f4f4;
+}
+
+h1 {
+    color: #333;
+}
+
+ul {
+    list-style: none;
+    padding: 0;
+}
+
+ul li {
+    margin: 10px 0;
+}
+
+a {
+    text-decoration: none;
+    color: blue;
+    font-size: 18px;
+}
